0400d80b8f3fc03a99066ab1bd994c9c083e3d05
Oleg Golovchenko <IgRoK_be@hotmail.com>
2020-02-15 12:27:49 +0100

    added telemetryIps to getuser method

--- org.igrok-net.telemetry/Controllers/AdministrationController.cs
+++ org.igrok-net.telemetry/Controllers/AdministrationController.cs
@@ -135,6 +135,25 @@ namespace org.igrok_net.telemetry.Controllers
                         OsVersion = telemetry.OsVersion
                     };
                 }
+                var resultReader = _dataProvider.ExecuteReader($"SELECT COUNT(*) FROM telemetryIps WHERE telemetryId = {user.Id}");
+                var ips = new List<TelemetryIpModel>();
+                if (resultReader.HasRows)
+                {
+                    resultReader.Read();
+                    ips.Add(new TelemetryIpModel
+                    {
+                        Ip = resultReader.GetString(2)
+                    });
+                    while (resultReader.Read())
+                    {
+                        ips.Add(new TelemetryIpModel
+                        {
+                            Ip = resultReader.GetString(2)
+                        });
+                    }
+                    resultReader.Close();
+                }
+                result.Telemetry.TelemetryIps = ips;
                 return Ok(result);
             }
             catch (Exception e)
